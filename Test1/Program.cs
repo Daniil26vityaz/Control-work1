9a5da566e6bd0a25eedc4a437b236da8e59fe2fd
Daniil <mr.varfolomeev@bk.ru>
2023-08-30 19:38:32 +0300

    Закончил написание программы для решения контрольного задания

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -5,14 +5,26 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Clear();
         int size = MyLibClass.Input("Введите размер массива: ");
         string[] array = new string[size];
         Console.Write("Введите элементы массива: ");
         for (int i = 0; i < size; i++)
         {
             array[i] = Console.ReadLine();
-        }       
+        }
         string str = string.Join(", ", array);
-        Console.Write($"Ваш массив: [{str}]");
+        Console.WriteLine($"Ваш массив: [{str}]");
+        Console.Write($"Ваш новый массив : [");
+        for (int i = 0; i < size; i++)
+        {
+            if (array[i].Length <= 3)
+            {
+                Console.Write($"{array[i]}, ");
+            }
+        }
+        Console.Write("\b\b");
+        Console.Write($"]");
+
     }
 }
