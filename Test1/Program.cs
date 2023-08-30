3288f7e6cb4a2884f7f026846e8bc5a29759b975
Daniil <mr.varfolomeev@bk.ru>
2023-08-30 19:00:00 +0300

    Создал массив из строк, который заполняет пользователь

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace Test1;
+﻿using MyLib;
+namespace Test1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int size = MyLibClass.Input("Введите размер массива: ");
+        string[] array = new string[size];
+        Console.Write("Введите элементы массива: ");
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = Console.ReadLine();
+        }       
+        string str = string.Join(", ", array);
+        Console.Write($"Ваш массив: [{str}]");
     }
 }
